74b797fa74b048e8d44ae942d82d5f1ad4e32105
Shruddha Bhat <shruddha.bhat@precisely.com>
2026-04-25 22:22:31 +0530

    Check deployment replica

--- golang/main.go
+++ golang/main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"net/http"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	kubeconfig := flag.String("kubeconfig", "", "path to kubeconfig, leave empty for in-cluster")
-	listenAddr := flag.String("address", ":8080", "HTTP server listen address")
+	//	listenAddr := flag.String("address", ":8080", "HTTP server listen address")
 
 	flag.Parse()
 
@@ -32,9 +34,14 @@ func main() {
 
 	fmt.Printf("Connected to Kubernetes %s\n", version)
 
-	if err := startServer(*listenAddr); err != nil {
-		panic(err)
+	if _, err := checkDeploymentReplicas(clientset); err != nil {
+		fmt.Printf("%v\n", err)
 	}
+
+	//	if err := startServer(*listenAddr); err != nil {
+	//		panic(err)
+	//	}
+
 }
 
 // getKubernetesVersion returns a string GitVersion of the Kubernetes server defined by the clientset.
@@ -68,4 +75,24 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println("failed writing to response")
 	}
+}
+
+// Check Deployment Replicas and compare with desired count, return an error if they don't match.
+func checkDeploymentReplicas(clientset kubernetes.Interface) (string, error) {
+	contxt := context.Background()
+	namespace := "" //all namespaces by default
+
+	alldeployments, err := clientset.AppsV1().Deployments(namespace).List(contxt, metav1.ListOptions{})
+	if err != nil {
+		return "", err
+	}
+
+	for _, deployment := range alldeployments.Items {
+		if *deployment.Spec.Replicas != deployment.Status.AvailableReplicas {
+
+			fmt.Printf("WARNING: Deployment replica check found a mismatch \n %v", err)
+			return "", fmt.Errorf("NAMESPACE:%s, NAME:%s, EXPECTED-REPLICAS:%d, AVAILABLE-REPLICAS:%d", deployment.Namespace, deployment.Name, *deployment.Spec.Replicas, deployment.Status.AvailableReplicas)
+		}
+	}
+	return "", nil
 }
